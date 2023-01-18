44e2b19843d6ebf2933db0f136cc79fdccf5b2ff
Alex Jercan <jercan_alex27@yahoo.com>
2023-01-18 14:28:51 +0200

    feat: stolen from tj

--- nvim/.config/nvim/lua/theprimeagen/init.lua
+++ nvim/.config/nvim/lua/theprimeagen/init.lua
@@ -1,15 +1,12 @@
 require("theprimeagen.set")
 require("theprimeagen.remap")
+require("theprimeagen.globals")
 
 local augroup = vim.api.nvim_create_augroup
 local ThePrimeagenGroup = augroup('ThePrimeagen', {})
 
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
-
-function R(name)
-    require("plenary.reload").reload_module(name)
-end
 
 autocmd('TextYankPost', {
     group = yank_group,
